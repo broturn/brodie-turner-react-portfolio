19bae07e1a6753310d455c5498676455c32a2b77
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-07-10 14:26:19 -0500

    updates

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 
+import PortfolioContainer from "./portfolio/portfolio-container";
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Brodie Turner Portfolio</h1>
+                      
        <div>
        {moment().format('MMMM Do YYYY, h:mm:ss a')}
        </div>
+       <PortfolioContainer/>
        </div>
     );
   }
