9d361c055fdc107c5b1f7bd587efa98b995b2ea8
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-07-11 15:04:03 -0500

    added navlink

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
 import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+
+
+
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
+      <div className='app'>       
+        <Router>
+        <div>
+        <NavigationContainer />
+        <Switch>
+          <Route exact path="/" component={Home} />  
+          <Route path="/about-me" component={About} /> 
+          <Route path="/contact" component={Contact} />  
+          <Route path="/blog" component={Blog} />       
+          </Switch>
+        </div>
+        </Router>
+
         <h1>Brodie Turner Portfolio</h1>
                       
        <div>
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,6 +1,6 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-import PortfolioItem from './portfolio-item';
+import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
     constructor() {
@@ -8,27 +8,54 @@ export default class PortfolioContainer extends Component {
 
         this.state = {
             pageTitle: "Welcome to my portfolio",
-            data:[
-                {title:"Quip"}, 
-                {title:"Evenbrite"},
-                {title:"Ministry Safe"}
+            isLoading: false,
+            data: [
+                { title: "Quip", category: "eCommerce" }, 
+                { title: "Eventbrite", category: "Scheduling" },
+                { title: "Ministry Safe", category: "Enterprise" }
             ]
         };
+
+       this.handleFilter = this.handleFilter.bind(this);
     }
 
-    portfolioItems(){
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.category === filter;
+            })
+        });
+    }
+
+    portfolioItems() {
         return this.state.data.map(item => {
-            return <PortfolioItem title ={item.title} url={"google.com"}/>;
+            return <PortfolioItem title={item.title} url={"google.com"}/>;
         });
 
     }
 
     render() {
+        if (this.state.isLoading){
+            return <div>Loading...</div>
+        }
+
         return (
             <div>
                 <h2>{this.state.pageTitle}</h2>
 
+                <button onClick={ () => this.handleFilter('eCommerce')}>
+                    eCommerce
+                    </button>
+                <button onClick={ () => this.handleFilter('Scheduling')}>
+                    Scheduling
+                    </button>
+                <button onClick={ () => this.handleFilter('Enterprise')}>
+                    Enterprise
+                    </button>
+
                 {this.portfolioItems()}
+
+               
             </div>
         );        
     }
