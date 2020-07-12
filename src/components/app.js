7395b97aa2302fa13b7df38029d8b79d67d78f2b
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-07-11 19:35:24 -0500

    updates

--- src/components/app.js
+++ src/components/app.js
@@ -1,43 +1,36 @@
-import React, { Component } from 'react';
-import moment from 'moment';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
+import React, { Component } from "react";
+import moment from "moment";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
-
-
-
+import PortfolioDetail from "./portfolio/portfolio-detail";
+import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>       
+      <div className="app">
         <Router>
-        <div>
-        <NavigationContainer />
-        <Switch>
-          <Route exact path="/" component={Home} />  
-          <Route path="/about-me" component={About} /> 
-          <Route path="/contact" component={Contact} />  
-          <Route path="/blog" component={Blog} />       
-          </Switch>
-        </div>
-        </Router>
+          <div>
+            <h1>Brodie Turner Portfolio</h1>
+            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+            <NavigationContainer />
 
-        <h1>Brodie Turner Portfolio</h1>
-                      
-       <div>
-       {moment().format('MMMM Do YYYY, h:mm:ss a')}
-       </div>
-       <PortfolioContainer/>
-       </div>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
     );
   }
 }
