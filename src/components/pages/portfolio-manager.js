ba06e14defd55abcd79149a36e1ca4bab5bd58a0
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-07-16 14:32:47 -0500

    edit icon added to portfolio manager

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -9,12 +9,21 @@ export default class PortfolioManager extends Component {
         super();
 
         this.state = {
-            portfolioItems: []
+            portfolioItems: [],
+            portfolioToEdit: {}
         };
         this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
         this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
         this.handleDeleteClick = this.handleDeleteClick.bind(this);
+        this.handleEditClick = this.handleEditClick.bind(this);
     }
+    
+    handleEditClick(portfolioItem) {
+       this.setState({
+           portfolioToEdit: portfolioItem
+       }) 
+    }
+
     handleDeleteClick(portfolioItem) {
         axios.delete(`https://api.devcamp.space/portfolio/portfolio_items/${portfolioItem.id}`, { withCredentials: true}).then(response => {
             this.setState({
@@ -69,7 +78,9 @@ export default class PortfolioManager extends Component {
                 <div className="right-column">
                    <PortfolioSidebarList 
                    handleDeleteClick={this.handleDeleteClick}
-                   data={this.state.portfolioItems}/>
+                   data={this.state.portfolioItems}
+                   handleEditClick={this.handleEditClick}
+                   />
                 </div>
 
             </div>
