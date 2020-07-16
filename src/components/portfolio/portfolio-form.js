9618c88f995fa1de5861c8f606780e975a83f235
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-07-16 16:54:33 -0500

    edited items update in sidebar without refresh

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -136,9 +136,12 @@ export default class PortfolioForm extends Component {
         withCredentials: true
        })
              .then(response => {
-                 this.props.handleSuccessfulFormSubmission(response.data.portfolio_item);
-
-                
+                 if (this.state.editMode) {
+                     this.props.handleEditFormSubmission();
+                 }else {
+                     this.props.handleNewFormSubmission(response.data.portfolio_item)
+                 }
+                                
                  this.setState ({
                     name: "",
                     description: "",
@@ -147,7 +150,10 @@ export default class PortfolioForm extends Component {
                     url: "",
                     thumb_image: "",
                     banner_image: "",
-                    logo: ""
+                    logo: "",
+                    editMode: false,
+                    apiUrl: "https://brodieturner.devcamp.space/portfolio/portfolio_items",
+                    apiAction: "post"
                 });
                 
                 [this.thumbRef, this.bannerRef, this.logoRef].forEach(ref => {
