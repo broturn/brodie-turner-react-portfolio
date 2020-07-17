1438daf69e68edaff14067e3445159a97dd7ee9a
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-07-17 17:33:21 -0500

    started populating blog components

--- src/components/app.js
+++ src/components/app.js
@@ -10,6 +10,7 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
+import BlogDetail from "./pages/blog-detail";
 import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
@@ -30,20 +31,20 @@ export default class App extends Component {
     this.handleSuccessfulLogout = this.handleSuccessfulLogout.bind(this);
   }
 
-  handleSuccessfulLogin () {
+  handleSuccessfulLogin() {
     this.setState({
       loggedInStatus: "LOGGED_IN"
 
     });
   }
   
-  handleUnsuccessfulLogin () {
+  handleUnsuccessfulLogin() {
     this.setState({
       loggedInStatus: "NOT_LOGGED_IN"
 
     });
   }
-  handleSuccessfulLogout () {
+  handleSuccessfulLogout() {
     this.setState({
       loggedInStatus: "NOT_LOGGED_IN"
 
@@ -116,6 +117,7 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
+              <Route path="/b/:slug" component={BlogDetail} />
               {this.state.loggedInStatus === "LOGGED_IN" ? this.authorizedPages() : null}
               <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
               <Route component={NoMatch} />
