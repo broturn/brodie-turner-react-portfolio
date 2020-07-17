4300e032a923944e766ff2b393425a412d280c89
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-07-17 12:24:53 -0500

    finished portfolio form

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -30,10 +30,24 @@ export default class PortfolioForm extends Component {
         this.handleThumbDrop = this.handleThumbDrop.bind(this);
         this.handleBannerDrop = this.handleBannerDrop.bind(this);
         this.handleLogoDrop = this.handleLogoDrop.bind(this);
+        this.deleteImage = this.deleteImage.bind(this);
 
         this.thumbRef = React.createRef();
         this.bannerRef = React.createRef();
         this.logoRef = React.createRef();
+    }
+
+    deleteImage(imageType) {
+        axios.delete(`https://api.devcamp.space/portfolio/delete-portfolio-image/${this.state.id}?image_type=${imageType}`, { withCredentials: true })
+        .then(response => {
+            this.setState({
+                [`${imageType}_url`]: ""
+            });
+        }) 
+        .catch(error => {
+            console.log("deleteImage error", error);
+        })
+        
     }
 
     componentDidUpdate() {
@@ -62,9 +76,9 @@ export default class PortfolioForm extends Component {
                 editMode: true,
                 apiUrl: `https://brodieturner.devcamp.space/portfolio/portfolio_items/${id}`,
                 apiAction: "patch",
-                thumb_image: thumb_image_url || "",
-                banner_image: banner_image_url || "",
-                logo: logo_url || ""
+                thumb_image_url: thumb_image_url || "",
+                banner_image_url: banner_image_url || "",
+                logo_url: logo_url || ""
             })
         }
     }
@@ -226,9 +240,15 @@ export default class PortfolioForm extends Component {
                     </div> 
                     <div className="image-uploaders">
                         
-                        {this.state.thumb_image && this.state.editMode ? ( 
+                        {this.state.thumb_image_url && this.state.editMode ? ( 
                         <div className="portfolio-manager-image-wrapper">
-                        <img src ={this.state.thumb_image} />
+                        <img src ={this.state.thumb_image_url} />
+
+                        <div className="image-removal-link">
+                            <a onClick={() => this.deleteImage("thumb_image")}>
+                                Remove File
+                            </a>
+                        </div>
                         </div>
                         ):(
 
@@ -243,9 +263,14 @@ export default class PortfolioForm extends Component {
                          </DropzoneComponent>
                            )}
 
-                        {this.state.banner_image && this.state.editMode ? (
+                        {this.state.banner_image_url && this.state.editMode ? (
                         <div className="portfolio-manager-image-wrapper">
-                        <img src ={this.state.banner_image} />
+                        <img src ={this.state.banner_image_url} />
+                        <div className="image-removal-link">
+                            <a onClick={() => this.deleteImage("banner_image")}>
+                                Remove File
+                            </a>
+                        </div>
                         </div>
                         ):(
                      
@@ -258,9 +283,14 @@ export default class PortfolioForm extends Component {
                                 <div className= "dz-message">Banner</div>
                          </DropzoneComponent>
                          ) }
-                            {this.state.logo && this.state.editMode ? (
+                            {this.state.logo_url && this.state.editMode ? (
                         <div className="portfolio-manager-image-wrapper">
-                        <img src ={this.state.logo} />
+                        <img src ={this.state.logo_url} />
+                        <div className="image-removal-link">
+                            <a onClick={() => this.deleteImage("logo")}>
+                                Remove File
+                            </a>
+                        </div>
                         </div>
                             ):(
 
