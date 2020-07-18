a265974b35535ea301f68295f3380823946a5632
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-07-17 19:13:59 -0500

    started adding styles to blogs

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -13,7 +13,9 @@ export default class BlogDetail extends Component {
     getBlogItem() {
         axios.get(`https://brodieturner.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`)
         .then(response => {
-            console.log('response', response);
+            this.setState({
+                blogItem: response.data.portfolio_blog
+            });
             
         }).catch(error =>{
             console.log('getBlogItem Error', error);
@@ -25,11 +27,24 @@ export default class BlogDetail extends Component {
         this.getBlogItem();
     }
     render() {
-        console.log('currentId', this.state.currentId);
+        const {
+            title,
+            content,
+            featured_image_url,
+            blog_status
+        }= this.state.blogItem;
+     
         
         return (
-            <div>
-                <h1>Blog detail</h1>
+            <div className="blog-container">
+                <div className="content-container">
+                <h1>{title}</h1>
+
+                <div className="featured-image-wrapper">
+                    <img src={featured_image_url}/>
+                </div>
+                <div className="content">{content}</div>
+                </div>
             </div>
         );
     }
--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -33,11 +33,14 @@ class Blog extends Component {
         return <BlogItem key={blogItem.id} blogItem={blogItem} />;
         })
 
+       
         return(
-            <div>
-               {blogRecords}
+            <div className="blog-container">
+                <div className="content-container">
+                {blogRecords}
+                </div>
             </div>
-            );
+        )
         }
     }
 
