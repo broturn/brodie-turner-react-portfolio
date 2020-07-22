532357fc77a21c764a7df0c2e74e982c0f666d41
Brodie Turner <64994857+broturn@users.noreply.github.com>
2020-07-22 12:41:07 -0500

    contact page updates

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import contactPagePicture from "../../../static/assets/images/auth/login.jpg";
+
 
 export default function() {
     return(
-        <div>
-        Contact
+        <div className="content-page-wrapper">
+        <div className="left-column" 
+        style={{
+            background: "url(" + contactPagePicture + ") no-repeat",
+            backgroundSize: "cover",
+            backgroundPostion: "cover"
+        }}
+        ></div>
+        <div className="right-column">
+            <div className="contact-bullet-points">
+                <div className="bullet-point-group">
+                    <div className="icon">
+                    <FontAwesomeIcon icon="mobile" />
+                    </div>
+                    <div className="text">
+                        555-555-5555
+                    </div>
+                </div>
+                    <div className="bullet-point-group">
+                    <div className="icon">
+                    <FontAwesomeIcon icon="envelope-square" />
+                    </div>
+                    <div className="text">
+                        broturn@gmail.com
+                    </div>
+                    </div>
+                    <div className="bullet-point-group">
+                    <div className="icon">
+                    <FontAwesomeIcon icon="map-pin" />
+                    </div>
+                    <div className="text">
+                        Lafayette,LA
+                    </div>
+                </div>
+
+            </div>
+        </div>
         </div>
     );
 }
--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -1,8 +1,8 @@
-import { faTrash, faSignOutAlt,faEdit,faSync,faFeatherAlt } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faSignOutAlt,faEdit,faSync,faFeatherAlt,faMobile,faMapPin,faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
 const Icons = () => {
-return library.add(faTrash, faSignOutAlt, faEdit,faSync, faFeatherAlt);
+return library.add(faTrash, faSignOutAlt, faEdit,faSync, faFeatherAlt,faMobile,faMapPin,faEnvelopeSquare);
 };
 
 export default Icons;
